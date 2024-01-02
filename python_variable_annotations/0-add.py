90ddc744314634a91dba62403194a9b3d9631805
JoseMilans <josemilans@gmail.com>
2024-01-02 23:06:01 +0000

    feat: Add function with type annotations for adding floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function add that takes two float
+arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the sum as a float.
+    """
+    return a + b
