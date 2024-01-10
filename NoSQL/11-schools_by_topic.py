bc6ba169a8b434d68adb5bc3719ee45c62c06a45
JoseMilans <josemilans@gmail.com>
2024-01-10 12:31:08 +0000

    feat: Add function to find schools by topic in MongoDB

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for finding schools with a specific topic in MongoDB
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of schools having a specific topic.
+    """
+    return list(mongo_collection.find({"topics": topic}))
